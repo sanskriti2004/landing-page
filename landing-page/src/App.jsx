5cb8543eae3abb1b9f1fe73266c279f94ca729bf
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-04-28 17:05:03 +0530

    initial commit

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from "./pages/Profile";
+import Landing from "./pages/Landing";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+function App() {
+  return (
+    <>
+      <Landing />
+      <Signup />
+      <Login />
+      <Profile />
+    </>
+  );
+}
+
+export default App;
---
+++ landing-page/src/pages/Landing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <div className="bg-[#F7F8F9]">
+        <div>
+          <h1 className="text-[#1D2226]">Welcome to PopX</h1>
+          <p className="text-[#1D2226] opacity-[60%]">
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit,
+          </p>
+        </div>
+        <div>
+          <button>Create Account</button>
+          <button>Already Registered? Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
