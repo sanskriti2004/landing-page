53d8c0e3afcc335d82a69f4f286daba4debd2b37
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-04-28 17:40:24 +0530

    added landing page

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -2,13 +2,18 @@ import Profile from "./pages/Profile";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Landing />
-      <Signup />
-      <Login />
-      <Profile />
+      <div className="flex justify-center h-screen items-center bg-white">
+        <Routes>
+          <Route element={<Profile />} path="/profile" />
+          <Route element={<Signup />} path="/signup" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Landing />} path="/" />
+        </Routes>
+      </div>
     </>
   );
 }
--- landing-page/src/pages/Landing.jsx
+++ landing-page/src/pages/Landing.jsx
@@ -1,18 +1,45 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 100);
+  };
+
+  const handleSignupClick = () => {
+    setTimeout(() => {
+      navigate("/signup");
+    }, 100);
+  };
+
   return (
     <div>
-      <div className="bg-[#F7F8F9]">
-        <div>
-          <h1 className="text-[#1D2226]">Welcome to PopX</h1>
-          <p className="text-[#1D2226] opacity-[60%]">
+      <div className="bg-[#F7F8F9] w-[375px] h-[812px] mx-auto shadow-lg overflow-auto flex flex-col justify-end ">
+        <div className="mb-[25px] ml-[20px] ">
+          <h1 className="text-[#1D2226] text-[28px] font-medium">
+            Welcome to PopX
+          </h1>
+          <p className="text-[#1D2226] opacity-[60%] text-[18px]   ">
             Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit,
           </p>
         </div>
-        <div>
-          <button>Create Account</button>
-          <button>Already Registered? Login</button>
+        <div className="flex flex-col justify-center items-center mb-[41px] ">
+          <button
+            onClick={handleSignupClick}
+            className="text-[18px] cursor-pointer w-[335px] h-[46px] bg-[#6C25FF] rounded-[6px] opacity-100"
+          >
+            Create Account
+          </button>
+          <button
+            onClick={handleLoginClick}
+            className="text-[18px] cursor-pointer w-[335px] h-[46px] bg-[#6C25FF4B] rounded-[6px] mt-[10px] opacity-100"
+          >
+            Already Registered? Login
+          </button>
         </div>
       </div>
     </div>
